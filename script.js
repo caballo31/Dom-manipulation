828db0d95f48f672d352c76448f9b4e2969b7800
caballo31 <versallialejandro@gmail.com>
2023-01-20 15:45:16 -0300

    Eventlister works

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 const calculo1 = document.querySelector("#calculo1");
 const calculo2 = document.querySelector("#calculo2");
-const btnCalcular = document.querySelector("btnCalcular");
+const btnCalcular = document.querySelector("#btnCalcular");
 const pResult = document.querySelector("#sumResult");
+
+btnCalcular.addEventListener('click', btnOnClick);
 
 function btnOnClick(){
     var number1 = Number(calculo1.value);
