4af94cfc40346171c0356fc5a52a5f222f939cac
caballo31 <versallialejandro@gmail.com>
2023-01-20 15:12:02 -0300

    Code works onclick

---
+++ script.js
@@ -0,0 +1,13 @@
+//  Crear variables para los elementos
+
+const calculo1 = document.querySelector("#calculo1");
+const calculo2 = document.querySelector("#calculo2");
+const btnCalcular = document.querySelector("btnCalcular");
+const pResult = document.querySelector("#sumResult");
+
+function btnOnClick(){
+    var number1 = Number(calculo1.value);
+    var number2 = Number(calculo2.value);
+    var sum = number1 + number2;
+    pResult.innerText = "Resultado: " + sum;
+}
